ff3bd95d4ea2e742130d95098987fc9dab2f1c66
Tommy <tommy@iamnotagoodartist.com>
2024-05-19 21:35:05 -0700

    Copy deploy.sh from hardware_hustle

---
+++ deploy.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+{
+
+set -o errexit
+set -o errtrace
+
+dir="_site/docs"
+
+function help() {
+    echo "\
+Deploys to GitHub pages.
+
+Usage:
+./deploy.sh
+./deploy.sh --do-it-live       Actually deploy
+"
+}
+
+function bonk() {
+    printf "\a"
+}
+
+function confirm_git_clean_status() {
+    echo "Checking git status for uncommitted changes"
+    if git_clean=$(git status --porcelain) && [ -z "$git_clean" ]; then
+        echo "Clean"
+        echo
+    else
+        echo "ERROR: Dirty."
+        exit
+    fi
+}
+
+function run() {
+    if [[ "$1" == *"-h"* ]]; then
+        help
+        exit
+    fi
+
+    confirm_git_clean_status
+
+    rm -rf $dir
+    mkdir -pv $dir >/dev/null
+
+    echo "BUILDING"
+    echo "--------"
+    npx @11ty/eleventy
+    echo
+
+    # TODO: fix /shopdocs URL stuff on github
+    echo "COMMITTING"
+    echo "----------"
+    git checkout gh-pages
+    mkdir -pv static
+    cp -r $dir/* "."
+    cp -r $dir/static/ "./static"
+    git add .
+    git commit -m "Deploy $dir"
+    echo
+
+    echo "DEPLOYING"
+    echo "---------"
+    if [[ "$1" == *"--do-it-live"* ]]; then
+        git push origin gh-pages
+    else
+        echo "Skipped! Supply --do-it-live flag to deploy."
+    fi
+
+    git checkout "@{-1}"
+
+    bonk
+}
+
+# Send full args string w/ spaces
+run "$*"
+
+}
